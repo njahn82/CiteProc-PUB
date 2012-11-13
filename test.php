55fa5e2355ec118f9c0c2ca3824a58bb497b0e14
jahn <jahn@pub-dev.ub.uni-bielefeld.de>
2012-11-13 15:22:05 +0100

    styles refact + conference ersatzer

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+foreach ($argv as $value){
+	$val_array = split("=",$value);
+	if($val_array[0] == 'input'){
+		$input['input'] = $val_array[1];
+	} elseif ($val_array[0] == 'style'){
+		$input['style'] = $val_array[1];
+	} elseif ($val_array[0] == 'format'){
+		$input['format'] = $val_array[1];
+	}
+}
+
+print_r($input);
+
+
+?>
